63ab3f1a7dd9e77104f56ae31e8f1a0544517ba0
Frank Laércio <franklaercio@gmail.com>
2020-08-03 20:30:02 -0300

    Create Teacher form and list

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+import PageHeader from '../../componets/PageHeader';
+
+function TeacherList(){
+    return  (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis."  />
+        </div>
+    )
+
+}
+
+export default TeacherList;
