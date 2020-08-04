962ab67078e37d8bfb3479e5571fd44fdb1009c2
Frank Laércio <franklaercio@gmail.com>
2020-08-03 21:52:18 -0300

    Create Techer Item and styles web pages

---
+++ web/src/componets/TeacherItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+function TeacherItem(){
+    return <article className="teacher-item">
+    <header>
+        <img src="https://avatars3.githubusercontent.com/u/38151364?s=460&u=e839b3579bd6b77ba25cc1f090c74f9ed55d1821&v=4" alt="Frank Laércio"/>
+        <div>
+            <strong>Frank Laércio</strong>
+            <span>Programação Orientada à Objectos</span>
+        </div>
+    </header>
+
+    <p>
+        Entusiata das melhores tecnologias de programação.
+        <br /> <br />
+        Apaixonada por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+    </p>
+
+    <footer>
+        <p>
+            Preço/hora 
+            <strong>R$ 20,00</strong>
+        </p>
+
+        <button type="button">
+            <img src={whatsappIcon} alt="whatsapp" />
+            Entrar em contato
+        </button>
+    </footer>
+</article>;
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import PageHeader from '../../componets/PageHeader';
 
 function TeacherForm() {
