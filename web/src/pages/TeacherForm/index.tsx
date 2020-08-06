269bddd1403f962e22b12145e4ecdd5bf0088480
Frank Laércio <franklaercio@gmail.com>
2020-08-05 23:33:22 -0300

    Adding data of form and styles

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 
 import PageHeader from '../../componets/PageHeader';
+import Input from '../../componets/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css';
+import Textarea from '../../componets/Textarea';
 
 function TeacherForm() {
     return (
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas." />
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que incrível que você quer dar aulas." 
+            description="O primeiro passo, é preencher esse formulário de inscrição."/>
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome Completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+
+                    <Textarea name="bio" label="Biografia" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
