9c7b90f937dfce8a363cbdc27fe830571721d5ff
Frank Laércio <franklaercio@gmail.com>
2020-08-17 23:37:12 -0300

    Adding search

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -4,11 +4,17 @@ import {View, ScrollView} from 'react-native';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import { TextInput } from 'react-native-gesture-handler';
 
 function TeacherList() {
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis" />
+            <PageHeader title="Proffys disponíveis" >
+                <View style={styles.searchForm}>
+                    <Text style={styles.label}>Matérias</Text>
+                    <TextInput style={styles.input} placeholder={styles.input}>Qual a matéria?</TextInput>
+                </View>
+            </PageHeader>    
 
             <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 24,}}>
                 <TeacherItem />
