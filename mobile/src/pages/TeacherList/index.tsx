df11253e6144f7c108d456143ec89c416e146627
Frank Laércio <franklaercio@gmail.com>
2020-08-18 01:40:48 -0300

    Finally style app

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, ScrollView, Text} from 'react-native';
+import { TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
+import {Feather} from '@expo/vector-icons';
+
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
-import { TextInput } from 'react-native-gesture-handler';
 
 function TeacherList() {
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    function handleToggleFiltersVisible(){
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
     return (
         <View style={styles.container}>
-            <PageHeader title="Proffys disponíveis" >
-                <View style={styles.searchForm}>
+            <PageHeader title="Proffys disponíveis" 
+            headerRight={<BorderlessButton onPress={handleToggleFiltersVisible}><Feather name="filter" size={20} color="#FFF"></Feather></BorderlessButton>}>
+                {isFiltersVisible && (<View style={styles.searchForm}>
                     <Text style={styles.label}>Matérias</Text>
                     <TextInput style={styles.input} placeholder="Qual a matéria?" placeholderTextColor="c1bccc" />
 
@@ -25,7 +34,11 @@ function TeacherList() {
                             <TextInput style={styles.input} placeholder="Qual o horário?" placeholderTextColor="c1bccc"/>
                         </View>
                     </View>
-                </View>
+
+                    <RectButton style={styles.submitButton}>
+                        <Text style={styles.textSubmitButton}>Filtrar</Text>
+                    </RectButton>
+                </View>)}
             </PageHeader>    
 
             <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 24,}}>
