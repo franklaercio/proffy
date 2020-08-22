5cd989271993719f8cc4586fc9ba5b88a48857eb
Frank Laércio <franklaercio@gmail.com>
2020-08-22 17:53:59 -0300

    Adding count connetions

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,6 +8,7 @@ import { RectButton } from 'react-native-gesture-handler';
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number,
@@ -29,6 +30,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({teacher, favorited}) => {
     const [isFavorited, setIsFavorited] = useState(favorited);
 
     function handleLinkToWhatsapp() {
+        api.post('connections', {user_id: teacher.id,});
         Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
     }
 
