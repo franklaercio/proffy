c91156a8a177b04ee9e236cd76ccaef5b75f42c7
Frank Laércio <franklaercio@gmail.com>
2020-08-22 17:01:48 -0300

    implementing search proffy

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,27 +8,41 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
-                <Image style={styles.avatar} source={{uri: 'https://avatars3.githubusercontent.com/u/38151364?s=460&u=e839b3579bd6b77ba25cc1f090c74f9ed55d1821&v=4'}} />
+                <Image style={styles.avatar} source={{uri: teacher.avatar}} />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}> Frank Laércio</Text>
-                    <Text style={styles.subject}> Programação </Text>
+                    <Text style={styles.name}> {teacher.name}</Text>
+                    <Text style={styles.subject}> {teacher.subject} </Text>
                 </View>
             </View>
 
             <Text style={styles.bio}>
-                Sometimes I am a student, teacher or doctor, because I solve problems by developing software.
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/Hora {' '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>{teacher.cost}</Text>
                 </Text>
 
                 <View style={styles.buttonsContainer}>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -6,10 +6,29 @@ import styles from './styles';
 import {Feather} from '@expo/vector-icons';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import api from '../../services/api';
 
 function TeacherList() {
     const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+    const [teachers, setTeachers] = useState([]);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekday] = useState('');
+    const [time, setTime] = useState('');
+
+    async function handleFiltersSubmit(){
+        const response = await api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
+    }
 
     function handleToggleFiltersVisible(){
         setIsFiltersVisible(!isFiltersVisible);
@@ -21,34 +40,30 @@ function TeacherList() {
             headerRight={<BorderlessButton onPress={handleToggleFiltersVisible}><Feather name="filter" size={20} color="#FFF"></Feather></BorderlessButton>}>
                 {isFiltersVisible && (<View style={styles.searchForm}>
                     <Text style={styles.label}>Matérias</Text>
-                    <TextInput style={styles.input} placeholder="Qual a matéria?" placeholderTextColor="c1bccc" />
+                    <TextInput style={styles.input} value={subject} onChangeText={text=> setSubject(text)} placeholder="Qual a matéria?" placeholderTextColor="c1bccc" />
 
                     <View style={styles.inputGroup}>
                         <View style={styles.inputBlock}>
                             <Text style={styles.label}>Dia da semana</Text>
-                            <TextInput style={styles.input} placeholder="Qual o dia?" placeholderTextColor="c1bccc"/>
+                            <TextInput style={styles.input} value={week_day} onChangeText={text=> setWeekday(text)} placeholder="Qual o dia?" placeholderTextColor="c1bccc"/>
                         </View>
 
                         <View style={styles.inputBlock}>
                             <Text style={styles.label}>Horário</Text>
-                            <TextInput style={styles.input} placeholder="Qual o horário?" placeholderTextColor="c1bccc"/>
+                            <TextInput style={styles.input} value={time} onChangeText={text=> setTime(text)} placeholder="Qual o horário?" placeholderTextColor="c1bccc"/>
                         </View>
                     </View>
 
-                    <RectButton style={styles.submitButton}>
+                    <RectButton style={styles.submitButton} onPress={handleFiltersSubmit}>
                         <Text style={styles.textSubmitButton}>Filtrar</Text>
                     </RectButton>
                 </View>)}
             </PageHeader>    
 
             <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 24,}}>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher}/>;
+                })}
             </ScrollView>
         </View>
     );
